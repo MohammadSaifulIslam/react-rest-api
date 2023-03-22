6ca3baaec6e4643b8a2b06ebe2b476cf8ac3568f
MohammadSaifulIslam <saifmdislam231@gmail.com>
2023-03-22 14:47:03 +0600

    grid layout use in react and add unique key practice

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Country from '../Country/Country';
-
+import './Countries.css'
 const Countries = () => {
     const [ countries , setCountries] = useState([])
     useEffect(()=> {
@@ -11,12 +11,16 @@ const Countries = () => {
     return (
         <div>
             <h1>Total Countries: {countries.length} </h1>
+            <div className="country-container">
+
             {
                 countries.map(country => <Country 
                     country={country}
+                    key={country.cca3}
                     ></Country>)
 
             }
+            </div>
         </div>
     );
 };
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Country.css'
 const Country = (props) => {
-    const {name,capital,population,area} = props.country;
-    // console.log(props)
+    const {name, capital ,population, area, flags} = props.country;
+    console.log(props.country) 
     return (
         <div className='container'>
             <h3>Country Name: {name.common}</h3>
+            <img src={flags.png} alt="falg" />
             <h5>Country Capital: {capital}</h5>
             <p>Country Population: {population}</p>
             <p>Country Area: {area}</p>
