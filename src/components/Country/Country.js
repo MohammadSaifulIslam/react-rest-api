73e42af5b4546fff2a6ce335e673e9a65dafd4ce
MohammadSaifulIslam <saifmdislam231@gmail.com>
2023-03-22 07:38:36 +0600

    show single country data in ui by destructuring object

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
-const Country = () => {
+import './Country.css'
+const Country = (props) => {
+    const {name,capital,population,area} = props.country;
+    // console.log(props)
     return (
-        <div>
-            <h1>Hello from Country</h1>
+        <div className='container'>
+            <h3>Country Name: {name.common}</h3>
+            <h5>Country Capital: {capital}</h5>
+            <p>Country Population: {population}</p>
+            <p>Country Area: {area}</p>
         </div>
     );
 };
